21702e8097a4454a06ddb9c6b00db9be8cb96825
Adrian Holland <hollandar@gmail.com>
2024-05-16 14:31:12 +1000

    Profile Set and Get Endpoints

---
+++ Shared.Validation/ValidationExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+using FluentValidation.Results;
+
+namespace Shared.Validation;
+
+public static class ValidationExtensions
+{
+    public static Result AsResult(this ValidationResult validationResult)
+    {
+        if (validationResult.IsValid) {
+            return Result.Ok();
+        } else
+        {
+            return Result.Fail(string.Join(", ", validationResult.Errors.Select(r => r.ErrorMessage).ToArray()));
+        }
+       
+    }
+}
