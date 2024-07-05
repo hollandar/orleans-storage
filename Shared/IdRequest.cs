acf313ef9448682ac41214532531994da9aaf592
Adrian Holland <hollandar@gmail.com>
2024-07-05 12:08:35 +1000

    Multiple report types

--- Shared/IdRequest.cs
+++ Shared/IdRequest.cs
@@ -2,21 +2,59 @@
 
 public class IdRequest<T0>
 {
+    public IdRequest()
+    {
+        
+    }
+    public IdRequest(T0 id0)
+    {
+        this.Id = id0;
+    }
+
     public T0 Id { get; set; } = default(T0)!;
 }
 
 public class IdRequest<T0, T1> : IdRequest<T0>
 {
+    public IdRequest():base()
+    {
+        
+    }
+
+    public IdRequest(T0 id0, T1 id1): base(id0)
+    {
+        this.Id1 = id1;
+    }
+
     public T1 Id1 { get; set; } = default(T1)!;
 }
 
 public class IdRequest<T0, T1, T2> : IdRequest<T0, T1>
 {
+    public IdRequest():base()
+    {
+        
+    }
+
+    public IdRequest(T0 id0, T1 id1, T2 id2): base(id0, id1)
+    {
+        this.Id2 = id2;
+    }
+
     public T2 Id2 { get; set; } = default(T2)!;
 }
 
 public class IdRequest<T0, T1, T2, T3> : IdRequest<T0, T1, T2>
 {
+    public IdRequest() : base()
+    {
+
+    }
+
+    public IdRequest(T0 id0, T1 id1, T2 id2, T3 id3) : base(id0, id1, id2)
+    {
+        this.Id3 = id3;
+    }
     public T3 Id3 { get; set; } = default(T3)!;
 }
 
