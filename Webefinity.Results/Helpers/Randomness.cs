01685f0d424ab00de2c02276a0607e292e3575ac
Adrian Holland <hollandar@gmail.com>
2024-08-01 11:58:14 +1000

    Fix dependeny references

---
+++ Webefinity.Results/Helpers/Randomness.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Webefinity.Results.Helpers;
+
+public static class Randomness
+{
+    public static string RandomString(int length, string? baseCharacters = null)
+    {
+        string chars = baseCharacters ?? "abcdefghijklmnopqrstuvwxyz0123456789";
+        var builder = new StringBuilder();
+        for (int i = 0; i < length; i++)
+        {
+            builder.Append(chars[RandomNumberGenerator.GetInt32(chars.Length)]);
+        }
+
+        Debug.Assert(builder.Length == length);
+        return builder.Length.ToString();
+    }
+}
