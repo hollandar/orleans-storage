424a4c678973e11a1f5548a446fe86e2abb6d648
Adrian Holland <hollandar@gmail.com>
2024-08-14 15:07:50 +1000

    Associate team and club @ creation

---
+++ Webefinity.Results/AssociateRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webefinity.Results;
+
+public struct AssociateRequest<TAssocitedId, TModel>
+{
+    public AssociateRequest()
+    {
+        
+    }
+
+    public AssociateRequest(TAssocitedId associtedId, TModel model)
+    {
+        this.AssociatedId = associtedId;
+        this.Model = model;
+    }
+
+    public TAssocitedId AssociatedId { get; set; }
+    public TModel Model { get; set; }
+}
