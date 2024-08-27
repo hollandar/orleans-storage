31f56301120a528b588c8f179173dc729308dff2
Adrian Holland <hollandar@gmail.com>
2024-08-27 13:57:59 +1000

    Post subscription pages

---
+++ Webefinity.ContentRoot.Hosting.Client/ContentRootClientService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webefinity.ContentRoot.Hosting.Client
+{
+    public class ContentRootClientService : IContentRootService
+    {
+        private HttpClient httpClient;
+
+        public ContentRootClientService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<ContentStream> GetStreamContentAsync(string collection, string file, CancellationToken ct)
+        {
+            var url = $"/content/{collection}/{file}";
+            var response = await httpClient.GetAsync(url, ct);
+            if (response.IsSuccessStatusCode)
+            {
+                return new ContentStream
+                {
+                    Success = true,
+                    Stream = response.Content.ReadAsStream(ct),
+                    ContentType = response.Content.Headers.Contains("Content-Type") ? response.Content.Headers.GetValues("Content-Type").First() : "application/octet-stream",
+                    StatusCode = response.StatusCode
+                };
+
+            }
+
+            else
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return new ContentStream
+                {
+                    Success = false,
+                    State = ContentState.NotFound,
+                    StatusCode = response.StatusCode
+                };
+            }
+
+            else
+
+            {
+                return new ContentStream
+                {
+                    Success = false,
+                    StatusCode = response.StatusCode
+                };
+            }
+        }
+    }
+}
---
+++ Webefinity.ContentRoot.Hosting/StartupExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Webefinity.ContentRoot.Hosting.Client;
+
+namespace Webefinity.ContentRoot.Hosting;
+
+public class ContentRootOptionsBuilder
+{
+    public string ConfigurationSection { get; set; } = "ContentRoot";
+}
+
+public static class StartupExtensions
+{
+    public static void AddContentRoot(this WebApplicationBuilder builder, Action<ContentRootOptionsBuilder>? action = null)
+    {
+        var crb = new ContentRootOptionsBuilder();
+        if (action is not null)
+        action(crb);
+
+        var options = new ContentRootOptions();
+        builder.Configuration.GetSection(crb.ConfigurationSection).Bind(options);
+
+        switch (options.Type)
+        {
+            case ContentRootType.File:
+                builder.Services.AddSingleton<IContentRootLibrary, ContentRootFile>();
+                break;
+            default:
+                throw new Exception("Content root type not supported.");
+        }
+
+        builder.Services.AddScoped<IContentRootService, ContentRootService>();
+    }
+
+    public static void MapContentRootServer(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/content/{Collection}/{**Path}", (string Collection, string Path, IContentRootLibrary contentRootLibrary) => {
+            var collectionDef = new CollectionDef(Collection);
+            if (contentRootLibrary.FileExists(collectionDef, Path))
+            {
+                return Results.Stream(contentRootLibrary.LoadReadStream(collectionDef, Path), ContentTypes.GetContentType(Path));
+            } else
+            {
+                return Results.NotFound("Content not found.");
+            }
+        });
+    }
+}
