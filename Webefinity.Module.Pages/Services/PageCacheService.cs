c415cc05fa8a5060ddf8c73bf2fc6b341c29e77d
Adrian Holland <hollandar@gmail.com>
2024-08-29 05:53:00 +1000

    Pages handling, and cache pages.

---
+++ Webefinity.Module.Pages/Services/PageCacheService.cs
@@ -0,0 +1,87 @@
+﻿
+using Webefinity.Results;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Webefinity.ContentRoot;
+using Webefinity.Module.Pages.Options;
+using Microsoft.Extensions.Options;
+
+namespace Webefinity.Module.Pages.Services;
+
+public record PageCache(
+    string Title,
+    string Content);
+
+public class PageFrontmatter
+{
+    public string Route { get; set; } = string.Empty;
+    public string? Title { get; set; } = null;
+}
+
+public class PageCacheService
+{
+    private readonly IServiceProvider serviceProvider;
+    private readonly IContentRootLibrary contentRootLibrary;
+    private readonly IOptions<PageRouterOptions> options;
+    private IMemoryCache? memoryCache;
+
+
+    public PageCacheService(IServiceProvider serviceProvider, IContentRootLibrary contentRootLibrary, IOptions<PageRouterOptions> options)
+    {
+        this.serviceProvider = serviceProvider;
+        this.contentRootLibrary = contentRootLibrary;
+        this.options = options;
+        memoryCache = this.serviceProvider.GetService<IMemoryCache>();
+    }
+
+    public async Task<ValueResult<PageCache>> GetPageAsync(string? route)
+    {
+        var pageRoute = $"/{route}";
+
+        if (this.memoryCache is not null && this.memoryCache.TryGetValue(pageRoute, out PageCache pageCache))
+        {
+            return ValueResult<PageCache>.Ok(pageCache);
+        }
+
+        var collection = new CollectionDef(options.Value.Collection);
+        await foreach (var page in contentRootLibrary.EnumerateRecursiveAsync(collection, "*.*"))
+        {
+            using var pageReader = contentRootLibrary.LoadReader(collection, page);
+            var frontmatterContent = await Frontmatter.FrontmatterLoader.LoadAsync<PageFrontmatter>(pageReader);
+
+            if (frontmatterContent.Frontmatter?.Route == pageRoute)
+            {
+                var title = frontmatterContent.Frontmatter.Title ?? route ?? "Page";
+                var content = string.Empty;
+                var contentType = ContentTypes.GetContentType(page);
+                switch (contentType)
+                {
+                    case "text/markdown":
+                        content = Markdig.Markdown.ToHtml(frontmatterContent.Content);
+                        break;
+                    case "text/htm":
+                    case "text/html":
+                        content = frontmatterContent.Content;
+                        break;
+                    default:
+                        throw new Exception($"Unsupported content type {contentType}.");
+
+                }
+
+                pageCache = new PageCache(title, content);
+
+                if (this.memoryCache is not null)
+                {
+                    this.memoryCache.Set(pageRoute, pageCache, new MemoryCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                    });
+                }
+
+                return ValueResult<PageCache>.Ok(pageCache);
+            }
+        }
+
+        return ValueResult<PageCache>.Fail("Page not found.", ResultReasonType.NotFound);
+    }
+}
--- Webefinity.Module.Pages/StartupExtensions.cs
+++ Webefinity.Module.Pages/StartupExtensions.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Webefinity.Module.Pages.Services;
 
 namespace Webefinity.Module.Blog
 {
     public static class StartupExtensions
     {
+        public static IServiceCollection AddWebefinityPages(this IServiceCollection services)
+        {
+            services.AddScoped<PageCacheService>();
+
+            return services;
+        }
+
         public static RazorComponentsEndpointConventionBuilder AddWebefinityPageComponents(this RazorComponentsEndpointConventionBuilder builder)
         {
             builder.AddAdditionalAssemblies(typeof(StartupExtensions).Assembly);
