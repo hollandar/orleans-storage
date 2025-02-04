dea032dc5230d38e09d878265d5fcfa2fd5db22a
Adrian Holland <hollandar@gmail.com>
2025-02-04 13:24:24 +1100

    Webefinity Extensions

--- Webefinity.Extensions/StringExtensions.cs
+++ Webefinity.Extensions/StringExtensions.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
-namespace Webefinity.Results.Extensions;
+namespace Webefinity.Extensions;
 
 public static partial class StringExtensions
 {
