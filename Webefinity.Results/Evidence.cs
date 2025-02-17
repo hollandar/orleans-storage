906356c2e3ced956a924ed7e8c21bfd51c454a3d
Adrian Holland <hollandar@gmail.com>
2025-02-17 12:43:08 +1100

    Evidence

---
+++ Webefinity.Results/Evidence.cs
@@ -0,0 +1,24 @@
+﻿namespace Webefinity.Results;
+
+public class Evidence<TEvidenceType>
+{
+    public Evidence()
+    {
+        IsSet = false;
+        Value = default(TEvidenceType);
+    }
+
+    public bool IsSet { get; set; }
+    public TEvidenceType? Value { get; set; }
+
+    public void Sign(TEvidenceType value)
+    {
+        if (IsSet)
+        {
+            throw new InvalidOperationException("Evidence already signed.");
+        }
+
+        IsSet = true;
+        Value = value;
+    }
+}
