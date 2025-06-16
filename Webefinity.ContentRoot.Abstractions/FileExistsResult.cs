b3c41d591914aa37c4a74d33d6f69bbc74ef0c64
Adrian Holland <hollandar@gmail.com>
2025-06-16 10:40:38 +1000

    Upload medical clearance

--- Webefinity.ContentRoot.Abstractions/FileExistsResult.cs
+++ Webefinity.ContentRoot.Abstractions/FileExistsResult.cs
@@ -2,12 +2,14 @@
 
 public class FileExistsResult
 {
-    public FileExistsResult(bool exists = false, string? etag = null) { 
+    public FileExistsResult(bool exists = false, string? etag = null, string? contentType = null) { 
         this.Exists = exists;
         this.ETag = etag;
+        this.ContentType = contentType;
     }
     public bool Exists { get; set; } = false;
     public string? ETag { get; set; }
+    public string? ContentType { get; set; }
 
     public static implicit operator bool(FileExistsResult result) => result.Exists;
     public static FileExistsResult False => new FileExistsResult(false);
