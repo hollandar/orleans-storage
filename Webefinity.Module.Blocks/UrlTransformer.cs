2ec3741a84d5878145ecdf398607dc0aef1b63a0
Adrian Holland <hollandar@gmail.com>
2026-07-30 12:47:28 +1000

    Remove ContentRoot projects and migrate to Cadence Upload
    Removed all Webefinity.ContentRoot.* references, code, and dependencies from solution and projects. Migrated file browser/upload features to Webefinity.Cadence.Upload packages. Updated UI components to use UriTransformer utility. Refactored FileManager and related components. Cleaned up obsolete code and updated build configuration.

---
+++ Webefinity.Module.Blocks/UrlTransformer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Webefinity.Module.Blocks
+{
+    public static class UriTransformer
+    {
+        public static string TransformSize(string url, string size = "", string defaultUrl = "unspecified.jpg")
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return defaultUrl;
+            }
+            return url;
+        }
+
+        public static string Transform(string url, string defaultUrl = "unspecified.jpg")
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return defaultUrl;
+            }
+            return url;
+        }
+    }
+}
